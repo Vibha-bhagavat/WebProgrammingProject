2fe4c317691a54c2d4a6b0aab1b62f2fd93cb24a
vibha <vibhapb02@gmailcom>
2022-12-07 14:07:39 -0500

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+/******************************************************************************
+***
+*  ITE5315 – Project
+*  I declare that this assignment is my own work in accordance with Humber Academic Policy.
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+*
+*  Group member Name:    Vibha Bhagavat 2.Rhea Christian
+                        Student IDs:    N01564677                     Date:07/12/2022
+******************************************************************************
+***/
 
 var express  = require('express');
 var mongoose = require('mongoose');
